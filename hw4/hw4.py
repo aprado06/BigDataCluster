5bb7adfcf8adfe627e2847f5867060ef455d076e
Airton Prado <airton.k.prado@gmail.com>
2018-04-26 13:19:30 -0400

    hw4

---
+++ hw4/hw4.py
@@ -0,0 +1,20 @@
+from pyspark import SparkContext
+
+if __name__ == '__main__'
+    sc = SparkContext()
+    RES_NYC ='/data/share/bdm/nyc_restaurants.csv'
+    Cus = sc.textFile(RES_NYC, use_unicode=False).cache()
+    list(enumerate(sat.first().split(',')))
+    
+    def extractCuisines(partId, records):
+    if partId==0:
+        records.next()
+    import csv
+    reader = csv.reader(records)
+    for row in reader:
+        (cuisine,count) = (row[7],1)
+        yield(cuisine,count)
+        
+    CusScores = Cus.mapPartitionsWithIndex(extractCuisines) \
+                .reduceByKey(lambda accum, n: accum + n) \
+                .saveAsTextFile('hw4output')
